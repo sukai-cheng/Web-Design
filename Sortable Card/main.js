cb4f6c80b0f3dd6d04e65eed8f880f6802f895bb
chengsukai <chengsukai@gmail.com>
2021-07-02 23:42:39 +0800

    2021/7/2 sortable card

--- Sortable Card/main.js
+++ Sortable Card/main.js
@@ -1,15 +1,19 @@
 const dropItems = document.getElementById('drop-items');
 
 new Sortable(dropItems, {
+	// 两个items交换效果
 	swap: true,
+	// 动画的持续时间
 	animation: 300,
+	// 排序
 	chosenClass: "sortable-chosen",
+	//drag效果
 	dragClass: "sortable-drag",
 
+	// 当drag的时候调整item的css样式
 	onMove: function (evt) {
 		var el = evt.dragged;
 		el.style.transform = 'rotate(-5deg)';
-		console.log(el.style.transform);
 	}
 });
 
