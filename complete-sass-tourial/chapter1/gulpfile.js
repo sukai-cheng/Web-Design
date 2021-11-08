41501c632dd04d7bc047a7477967b66f5fc3bdbe
承苏凯 <chengsukai@gmail.com>
2021-11-08 19:00:37 +0800

    学习sass

---
+++ complete-sass-tourial/chapter1/gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["shinobi/**/*.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
